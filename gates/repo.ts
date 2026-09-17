7f5987a33061f9be42ac7dddde2610953d6c15a9
Victor Taelin <victor.taelin@gmail.com>
2026-09-17 13:07:41 -0300

    The README cap is 3000 ttok

--- gates/repo.ts
+++ gates/repo.ts
@@ -32,7 +32,7 @@ allow(/^\.github\/ISSUE_TEMPLATE\/(bug|config)\.yml$/, 600);
 allow(".gitattributes", 200);
 allow(".gitignore", 100);
 allow("AGENTS.md", 2000);
-allow("README.md", 2500);
+allow("README.md", 3000);
 allow("LICENSE", 4000);
 allow("bend2/base.bend", 24000);
 allow("bend2/bend.lean", 400000);
