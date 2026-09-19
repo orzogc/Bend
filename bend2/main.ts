9dc916f0c9073be5efc483671e4e48fd60600f7f
Victor Taelin <victor.taelin@gmail.com>
2026-09-18 23:45:25 -0300

    Bend 2.0.13: IO.random_u32, File.read_at/size/write_bytes, the BOM survives on the JS lane, forky continuations run in place, a Nix flake, bend guide effects

--- bend2/main.ts
+++ bend2/main.ts
@@ -29,7 +29,7 @@ import * as Comp from "./comp.ts";
 // Constants
 // =========
 
-const VERSION = "2.0.12";
+const VERSION = "2.0.13";
 
 const HELP = `Bend ${VERSION}: check, run, build and publish Bend programs.
 
