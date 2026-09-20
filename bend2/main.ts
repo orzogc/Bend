2f08170bb2a0b726c261369aec8bd56285936a85
Victor Taelin <victor.taelin@gmail.com>
2026-09-19 22:59:44 -0300

    Bend 2.0.19: a fast start, a record of records, and a table for U32

--- bend2/main.ts
+++ bend2/main.ts
@@ -29,7 +29,7 @@ import * as Comp from "./comp.ts";
 // Constants
 // =========
 
-const VERSION = "2.0.18";
+const VERSION = "2.0.19";
 
 const HELP = `Bend ${VERSION}: check, run, build and publish Bend programs.
 
